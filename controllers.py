b4685ec74b5c9541007f5ea54164b69a9dd7f265
gabriel-leugaudas <gabriel.leugaudas@outlook.com>
2020-04-29 20:40:50 -0300

    Xesq

--- controllers.py
+++ controllers.py
@@ -1,16 +1,20 @@
 from aplicacao import app
 from flask import render_template
 
+
 @app.route('/home')
 def ola():
     return '<h1>Olá Mundo de Novo!</h1>'
+
 
 @app.route('/')
 def pagina_principal():
     return render_template('index.html')
 
+
 @app.route('/outra')
 def outra_pagina():
     return '<h1>Outra Pagina</h1>'
 
-app.run(debug=True)
+
+app.run()
--- venv/Scripts/activate.xsh
+++ venv/Scripts/activate.xsh
@@ -28,7 +28,7 @@ def _deactivate(args):
 _deactivate(["nondestructive"])
 aliases["deactivate"] = _deactivate
 
-$VIRTUAL_ENV = r"C:\Users\User\Desktop\xes\venv"
+$VIRTUAL_ENV = r"C:\Users\User\Desktop\Flask_aula\venv"
 
 $_OLD_VIRTUAL_PATH = $PATH
 $PATH = $PATH[:]
